ba885340779e468f7aa8cb3a747fe6054492839f
Milan Misak <misakmilan@gmail.com>
2020-03-28 12:51:21 +0000

    Change web app theme

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,23 @@
 import React from "react";
-import SignIn from './SignIn';
+import SignIn from "./SignIn";
+
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import green from "@material-ui/core/colors/green";
+import lightBlue from "@material-ui/core/colors/lightBlue";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: green,
+    secondary: lightBlue
+  }
+});
 
 function App() {
-  return <SignIn />;
+  return (
+    <ThemeProvider theme={theme}>
+      <SignIn />
+    </ThemeProvider>
+  );
 }
 
 export default App;
