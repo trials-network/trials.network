8d27ebba13fcc68038fd99f962d930b77b5d4482
Milan Misak <misakmilan@gmail.com>
2020-03-28 12:09:02 +0000

    Install prettier

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,8 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      trials.network
-    </div>
-  );
+  return <div className="App">trials.network</div>;
 }
 
 export default App;
