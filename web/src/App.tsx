65d5170f1f1913c446c7661710708b9301f716e5
Milan Misak <misakmilan@gmail.com>
2020-03-28 12:43:43 +0000

    Add a simple sign in page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import "./App.css";
+import SignIn from './SignIn';
 
 function App() {
-  return <div className="App">trials.network</div>;
+  return <SignIn />;
 }
 
 export default App;
