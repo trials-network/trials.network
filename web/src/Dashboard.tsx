7a8d8678e9cc7dde2c08f2e94aead1daa7e21520
Milan Misak <misakmilan@gmail.com>
2020-03-30 20:52:45 +0100

    Add a drawer

--- web/src/Dashboard.tsx
+++ web/src/Dashboard.tsx
@@ -1,35 +1,126 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link as RouterLink } from "react-router-dom";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import MenuIcon from "@material-ui/icons/Menu";
+import Drawer from "@material-ui/core/Drawer";
+import Divider from "@material-ui/core/Divider";
+import List from "@material-ui/core/List";
+import Container from "@material-ui/core/Container";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import DashboardIcon from "@material-ui/icons/Dashboard";
 import { makeStyles } from "@material-ui/core";
 
-const useStyles = makeStyles(() => ({
+const drawerWidth = 200;
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex"
+  },
+  toolbar: {
+    paddingRight: 24 // keep right padding when drawer closed
+  },
+  toolbarIcon: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    padding: "0 8px",
+    ...theme.mixins.toolbar
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    })
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  },
+  menuButton: {
+    marginRight: 36
+  },
+  menuButtonHidden: {
+    display: "none"
+  },
   title: {
     flexGrow: 1
+  },
+  drawerPaper: {
+    position: "relative",
+    whiteSpace: "nowrap",
+    width: drawerWidth,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  },
+  drawerPaperClose: {
+    overflowX: "hidden",
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    }),
+    width: theme.spacing(7),
+    [theme.breakpoints.up("sm")]: {
+      width: theme.spacing(9)
+    }
   },
   content: {
     flexGrow: 1,
     height: "100vh",
     overflow: "auto"
   },
-  bodyText: {
-    marginLeft: 30,
-    marginTop: 30
-  }
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4)
+  },
+  appBarSpacer: theme.mixins.toolbar
 }));
 
 export default function Dashboard() {
-  const classes = useStyles();
+  const classes = useStyles(),
+    [open, setOpen] = useState(false),
+    handleDrawerOpen = () => setOpen(true),
+    handleDrawerClose = () => setOpen(false);
+
   return (
-    <div>
+    <div className={classes.root}>
       <CssBaseline />
-      <AppBar position="static">
+      <AppBar
+        position="absolute"
+        className={`${classes.appBar} ${open ? classes.appBarShift : ""}`}
+      >
         <Toolbar>
-          <Typography variant="h6" className={classes.title}>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            className={`${classes.menuButton} ${
+              open ? classes.menuButtonHidden : ""
+            }`}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            component="h1"
+            variant="h6"
+            noWrap
+            className={classes.title}
+          >
             Dashboard
           </Typography>
           <Button component={RouterLink} to="/" color="inherit">
@@ -37,10 +128,39 @@ export default function Dashboard() {
           </Button>
         </Toolbar>
       </AppBar>
+      <Drawer
+        variant="permanent"
+        classes={{
+          paper: `${classes.drawerPaper} ${
+            !open ? classes.drawerPaperClose : ""
+          }`
+        }}
+        open={open}
+      >
+        <div className={classes.toolbarIcon}>
+          <IconButton onClick={handleDrawerClose}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          <div>
+            <ListItem button>
+              <ListItemIcon>
+                <DashboardIcon />
+              </ListItemIcon>
+              <ListItemText primary="Dashboard" />
+            </ListItem>
+          </div>
+        </List>
+      </Drawer>
       <main className={classes.content}>
-        <Typography variant="body1" className={classes.bodyText}>
-          Here we could have some stats/charts
-        </Typography>
+        <div className={classes.appBarSpacer} />
+        <Container maxWidth="lg" className={classes.container}>
+          <Typography variant="body1">
+            Here we could have some stats/charts
+          </Typography>
+        </Container>
       </main>
     </div>
   );
