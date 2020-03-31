381b439446aa337c8786ec8f906a1b98401bc3ce
Milan Misak <misakmilan@gmail.com>
2020-03-31 20:37:59 +0100

    Add basic add patient form

--- web/src/AddPatient.tsx
+++ web/src/AddPatient.tsx
@@ -1,6 +1,12 @@
-import React from "react";
-import Typography from "@material-ui/core/Typography";
+import React, { useState } from "react";
 import Container from "@material-ui/core/Container";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import FormControl from "@material-ui/core/FormControl";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormLabel from "@material-ui/core/FormLabel";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
 import { makeStyles } from "@material-ui/core";
 import Shell from "./components/Shell";
 
@@ -8,18 +14,87 @@ const useStyles = makeStyles(theme => ({
   container: {
     paddingTop: theme.spacing(4),
     paddingBottom: theme.spacing(4)
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1)
+  },
+  gender: {
+    marginTop: theme.spacing(2)
+  },
+  submit: {
+    margin: theme.spacing(2, 0)
   }
 }));
 
 export default function Dashboard() {
-  const classes = useStyles();
+  const classes = useStyles(),
+    [gender, setGender] = useState(null),
+    handleGenderChange = (e: any) => setGender(e.target.value);
 
   return (
     <Shell>
-      <Container maxWidth="lg" className={classes.container}>
-        <Typography variant="body1">
-          TODO
-        </Typography>
+      <Container maxWidth="sm" className={classes.container}>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="name"
+            label="Name"
+            name="name"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="id"
+            label="ID"
+            name="id"
+          />
+          <FormControl component="fieldset" className={classes.gender} required>
+            <FormLabel component="legend">Gender</FormLabel>
+            <RadioGroup
+              aria-label="gender"
+              name="gender"
+              value={gender}
+              onChange={handleGenderChange}
+            >
+              <FormControlLabel
+                value="female"
+                control={<Radio />}
+                label="Female"
+              />
+              <FormControlLabel value="male" control={<Radio />} label="Male" />
+              <FormControlLabel
+                value="other"
+                control={<Radio />}
+                label="Other"
+              />
+            </RadioGroup>
+          </FormControl>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="ethnicity"
+            label="Ethnicity"
+            name="ethnicity"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Add Patient
+          </Button>
+        </form>
       </Container>
     </Shell>
   );
